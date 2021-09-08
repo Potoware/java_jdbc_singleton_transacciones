6fc9a77ca12437c308596ee8ef568da440251b0e
Alejandro Potosi <alejandropotosi@outlook.com>
2021-09-08 11:24:28 -0500

    Independizando la conexion en Spring

--- src/main/java/com/potoware/java/jdbc/EjemploJdbc.java
+++ src/main/java/com/potoware/java/jdbc/EjemploJdbc.java
@@ -6,17 +6,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import com.potoware.java.jdbc.util.ConexionBaseDatos;
+
 public class EjemploJdbc {
 public static void main(String[] args) {
-	String url = "jdbc:mysql://localhost:3306/java_curso?serverTimezone=America/Bogota";
-	String username = "root";
-	String password = "sasa";
-
-	
-	try (Connection conn = DriverManager.getConnection(url, username,password);
-			Statement  stmt = conn.createStatement();
-			ResultSet resultado= stmt.executeQuery("SELECT * FROM productos");
-			){
+		
+	try (Connection conn = ConexionBaseDatos.getInstance();
+			Statement stmt = conn.createStatement();
+			ResultSet resultado = stmt.executeQuery("Select * from productos");){
 		
 		while (resultado.next()) {
 			System.out.print(resultado.getString("nombre")+" ");
