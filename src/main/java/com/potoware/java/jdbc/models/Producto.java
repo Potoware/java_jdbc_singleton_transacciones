243d670dc0ec017a9c5adfd216f9200cdcda7ec9
Alejandro Potosi <alejandropotosi@outlook.com>
2021-09-08 11:49:57 -0500

    Creando la interfaz repositorio

---
+++ src/main/java/com/potoware/java/jdbc/models/Producto.java
@@ -0,0 +1,11 @@
+package com.potoware.java.jdbc.models;
+
+import java.sql.Date;
+
+public class Producto {
+private Long id;
+private String nombreString;
+private Integer precio;
+private Date fechaRegistro;
+
+}
---
+++ src/main/java/com/potoware/java/jdbc/repositorio/Repositorio.java
@@ -0,0 +1,5 @@
+package com.potoware.java.jdbc.repositorio;
+
+public interface Repositorio {
+
+}
